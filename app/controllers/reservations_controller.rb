315da0452b80f91bb4be5aaeec58177c94336831
komiya7249 <komiyasakura@komiyasakura-no-MacBook-Pro.local>
2023-12-26 23:17:03 +0900

    施設予約確認時、リロードするとエラーになる点を修正

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,19 +17,23 @@ class ReservationsController < ApplicationController
   def confirm
     @reservation = Reservation.new(reservation_parms)
     @user = current_user
-    if @reservation.save
-      @room = @reservation.room
-      days_seconds = @reservation.check_out_at-@reservation.check_in_at
-      @reservation.days = (days_seconds/1.days).floor
-      @reservation.total_price = @reservation.persons*@room.price*@reservation.days 
-      @reservation.save
-    else
-      @room = @reservation.room
-      render"rooms/show"
-    end
+      if @reservation.valid? 
+        @room = @reservation.room
+        days_seconds = @reservation.check_out_at-@reservation.check_in_at
+        @reservation.days = (days_seconds/1.days).floor
+        @reservation.total_price = @reservation.persons*@room.price*@reservation.days 
+      else
+        @room = @reservation.room
+        render"rooms/show"
+      end
+      
   end
 
   def create
+    @reservation = Reservation.new(reservation_parms)
+    @user = current_user
+    @reservation.save
+    redirect_to :reservations
   end
 
   def edit
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,7 +14,7 @@ class Reservation < ApplicationRecord
   end
   
   def check_out_at_after
-    errors.add(:check_out_at,"はチェックイン日より後の日付を選択してください") if check_in_at.nil? || check_out_at.nil? || check_out_at < check_in_at
+    errors.add(:check_out_at,"はチェックイン日より後の日付を選択してください") if check_in_at.nil? || check_out_at.nil? || check_out_at <= check_in_at
   end 
 
 end
